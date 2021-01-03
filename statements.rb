f4b7e0b5c8b0804a8c5ceac019173cd15fcd6a57
droll-demo-7630 <danjamrhi@hotmail.com>
2021-01-03 19:18:01 +0000

    Automatically backed up by Learn

--- statements.rb
+++ statements.rb
@@ -1,1 +1,8 @@
-chance_of_rain = 0.atan
+chance_of_rain = 0.2 
+if chance_of_rain <= 0.25 
+  puts "Pack a sun shelter!"
+  elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75) 
+  puts "pack an umbrella!"
+else 
+  puts "Stay home and read Hegel."
+end 
