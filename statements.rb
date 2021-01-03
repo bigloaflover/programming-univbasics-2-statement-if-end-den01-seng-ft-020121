952182e49da0569fa1ac41933c5ee91ea49b27c9
droll-demo-7630 <danjamrhi@hotmail.com>
2021-01-03 19:09:00 +0000

    Automatically backed up by Learn

--- statements.rb
+++ statements.rb
@@ -2,6 +2,7 @@ chance_of_rain = 0.2
 puts "let's go outside!"
 if chance_of_rain < 0.5
   puts "pack an umbrella!"
-elseputs "Enjoy the fine day!"
+else
+  puts "Enjoy the fine day!"
 end
 puts "oh, and always wear sunscreen!"
