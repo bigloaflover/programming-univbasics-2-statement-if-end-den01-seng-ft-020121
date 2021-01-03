e07488735d6e9be87c51c4f51a793fe77f781548
droll-demo-7630 <danjamrhi@hotmail.com>
2021-01-03 19:07:51 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,5 +1,6 @@
 # Write your solution here
-chance_of_rain = 0.atan2puts "let's go outside!"
+chance_of_rain = 0.2 
+puts "let's go outside!"
 if chance_of_rain > 0.5
   puts "pack an umbrella!"
 elseputs "Enjoy the fine day!"
