442793e2095940259d9e81e8c637f1098d8217ee
droll-demo-7630 <danjamrhi@hotmail.com>
2021-01-03 19:06:07 +0000

    Automatically backed up by Learn

--- statements.rb
+++ statements.rb
@@ -1,1 +1,7 @@
 run_code_inside = false
+chance_of_rain = 0.atan2puts "let's go outside!"
+if chance_of_rain > 0.5
+  puts "pack an umbrella!"
+elseputs "Enjoy the fine day!"
+end
+puts "oh, and always wear sunscreen!"
