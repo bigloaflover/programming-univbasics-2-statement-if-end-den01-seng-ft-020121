f18a76b8e985c4852e27be1792bea8fe4f68ec2f
droll-demo-7630 <danjamrhi@hotmail.com>
2021-01-03 19:38:22 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,7 +1,8 @@
 # Write your solution here
-time = Time.now 
-if time == even 
-  print "Even!"
-else
-  print "Odd!"
-end
+current_time = Time.now 
+current_time = current_time.to_i  
+ if current_time % 2 == 0 
+   puts "Even!"
+ else
+   puts "Odd!"
+ end
